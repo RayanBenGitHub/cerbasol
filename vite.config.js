ef8235926e4e9db60338906798126cdb4665d4e4
Slashbens <70531180+Slashbens@users.noreply.github.com>
2025-02-13 16:55:50 +0100

    gh pages config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cerbasol/", 
+  base: "/cerbasol/", // Correspond au nom du dépôt GitHub
   build: {
     outDir: "dist",
   },
-};
+});
