198fd14e0bf22ac4f659fc8428d96ca164bbff80
Slashbens <70531180+Slashbens@users.noreply.github.com>
2025-02-13 17:31:29 +0100

    restauration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cerbasol/", // Correspond au nom du dépôt GitHub
-  build: {
-    outDir: "dist",
-  },
 });
